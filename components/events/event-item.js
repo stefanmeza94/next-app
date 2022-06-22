5ed78cff5b9c242d2f7198d59b2b311da5b032bf
Stefan Meza <stefanmeza93@gmail.com>
2022-06-22 13:54:06 +0200

    Add custom button component and finished styling

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,4 +1,7 @@
-import Link from 'next/link';
+import Button from '../ui/button';
+import DateIcon from '../icons/date-icon';
+import AddressIcon from '../icons/adress-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
 import classes from './event-item.module.css';
 
 function EventItem({ title, image, date, location, id }) {
@@ -19,15 +22,22 @@ function EventItem({ title, image, date, location, id }) {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableFormat}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
 
         <div className={classes.actions}>
-          <Link href={exploreLink}>Explore Event</Link>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
