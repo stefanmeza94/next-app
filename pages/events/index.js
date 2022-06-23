60d1324364c0e6f6a8bf58dbb21f1c6d624710cb
Stefan Meza <stefanmeza93@gmail.com>
2022-06-23 11:31:54 +0200

    Add submit form functionality

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,22 @@
 import { getAllEvents } from '../../dummy-data';
+import { useRouter } from 'next/router';
 import EventList from '../../components/events/event-list';
 import EventsSearch from '../../components/events/events-search';
 
 function AllEventsPage() {
   const events = getAllEvents();
 
+  const router = useRouter();
+
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  };
+
   return (
     <>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </>
   );
