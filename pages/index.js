9c34a80aff97bbbd35dbeb4d1292bb2a7d946671
Stefan Meza <stefanmeza93@gmail.com>
2022-06-30 16:44:02 +0200

    Add static generation for main index and dynamic instance of events pages

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,24 @@
-import { getFeaturedEvents } from '../dummy-data';
+import { getFeaturedEvents } from '../helpers/api-util';
 import EventList from '../components/events/event-list';
 
-function HomePage() {
-  const featuredEvents = getFeaturedEvents();
-
+function HomePage(props) {
   return (
     <div>
-      <EventList items={featuredEvents} />
+      <EventList items={props.events} />
     </div>
   );
 }
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+  };
+}
+
+// nastavi od 116 optimizing data fetching poglavlje 6
 
 export default HomePage;
